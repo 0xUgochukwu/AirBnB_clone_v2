b7eb2debf2ed9c57c2c7c5d2b1f612c1ec65924c
0xUgochukwu <0xUgochukwu@gmail.com>
2023-09-08 20:20:44 -0700

    setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static
+# Prep web server
 
-if [[ "$(which nginx; echo $?)" == '1' ]]; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
+if [[ "$(which nginx; echo $?)" == '0' ]];
+then
+	sudo apt-get update
+	sudo apt-get -y nginx
 fi
 
-sudo mkdir -p "/data/web_static/releases/test" "/data/web_static/shared"
-sudo echo 'Holberton School' > "/data/web_static/releases/test/index.html"
-sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
-sudo chown -hR ubuntu:ubuntu "/data"
+sudo mkdir /data/web_static/shared/ /data/web_static/releases/test/
+echo 'HBNB is coming!' > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
 
-static_block="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tindex index.html index.htm;\n\t\ttry_files \$uri \$uri/ =404;\n\t}"
-sudo sed -i "/error_page 404 /a${static_block}" "/etc/nginx/sites-available/default"
+hbnb="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tindex index.html index.htm;\n\t\ttry_files \$uri \$uri/ =404;\n\t}"
+sudo sed -i "/error_page 404 /a${hbnb}" "/etc/nginx/sites-available/default"
 sudo service nginx restart
