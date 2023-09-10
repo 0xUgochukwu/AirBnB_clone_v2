92fad09e282441bcc3d738fb421b8beb7dbc24e0
0xUgochukwu <0xUgochukwu@gmail.com>
2023-09-10 12:59:10 -0700

    pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-""" Module to pack files into .tgz """
-import os
-from datetime import datetime
-from fabric.api import runs_once, local
+'''
+A script to generate .tgz file from the contents of webstatic
+usage: fab -f 1-pack_web_static.py do_pack
+'''
+from fabric.api import local
+from time import strftime
 
 
-@runs_once
 def do_pack():
-    """ Archives web_static """
-    if os.path.exists('versions') or os.mkdir('versions'):
-        timestamp = datetime.now()
-        out_path = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-                timestamp.year,
-                timestamp.month,
-                timestamp.day,
-                timestamp.hour,
-                timestamp.minute,
-                timestamp.second
-                )
-
-        try:
-            print("Packing web_static to {}".format(out_path))
-            local("tar -cvzf {} web_static".format(out_path))
-            size = os.stat(output).st_size
-            print("web_static packed: {} -> {} Bytes".format(out_path, size))
-        except Exception:
-            out_path = None
-
-        return out_path
+    '''Generate required files'''
+    timenow = strftime('%Y%M%d%H%M%S')
+    try:
+        local('mkdir -p versions')
+        filename = 'versions/web_static_{}.tgz'.format(timenow)
+        local('tar -czvf {} web_static/'.format(filename))
+        return filename
+    except Exception:
+        return None
