43dd83ccb72db5f3d8cc6f7ef1358ce0fa800611
0xUgochukwu <0xUgochukwu@gmail.com>
2023-09-10 12:50:57 -0700

    prep

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # Prep web server
 
-if ! command -v nginx &> /dev/null;
-then
-	sudo apt-get update
-	sudo apt-get -y nginx
-	sudo service nginx start
-fi
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
 
-sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-echo 'HBNB is coming!' | sudo tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
+default_sites="/etc/nginx/sites-available/default"
+location=$(grep -Fn location $default_sites | head -1 | cut -d":" -f1)
+hbnb_static="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
-hbnb="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tindex index.html index.htm;\n\t\ttry_files \$uri \$uri/ =404;\n\t}"
-sudo sed -i "/error_page 404 /a${hbnb}" "/etc/nginx/sites-available/default"
+sudo mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"
+echo "<h1>Hello World!</h1>" | sudo tee "/data/web_static/releases/test/index.html"
+sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+sudo chown -hR ubuntu:ubuntu "/data/"
+
+sudo sed -i "${location}i ${hbnb_static}" "${default_sites}"
 sudo service nginx restart
