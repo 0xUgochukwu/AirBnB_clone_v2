551a948e47f2acfd1bbbeaafd34f340fed96ce96
0xUgochukwu <0xUgochukwu@gmail.com>
2023-09-24 03:46:56 -0700

    python

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,7 +21,7 @@ def c_text(text):
 
 
 @app.route('/python/<text>', strict_slashes=False)
-def py_text(text='cool'):
+def py_text(text='is cool'):
     return "Python {}".format(text.replace('_', ' '))
 
 
